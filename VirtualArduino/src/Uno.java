3620e61ffd28c1cc227e6cc789abd3229812863d
Ahmed Sabbah <ahmedsabba7@gmail.com>
2013-05-11 10:55:53 +0200

    program.hex file added to hold the code for uploading to Arduino, HexFileParser added which interprets the program code and save it to its location in flash memory, uploadprogram method added to Uno.java

--- VirtualArduino/src/Uno.java
+++ VirtualArduino/src/Uno.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Uno {
@@ -10,16 +12,18 @@ public class Uno {
 	Pin[] power;
 	Pin reset, aref,gnd;
 	boolean powerLed, txLed, rxLed, testLed;
-	int[][] flashMemory;
-	int[][] eeprom;
+	
+	byte[] flashMemory_program;
+	byte[] flashMemory_bootloader;
+	byte[] eeprom;
 	
 	// SRAM
-	int[][] registers;
-	int[][] IORegisters;
-	int[][] extIORegisters;
-	int[][] internalSram;
+	byte[] registers;
+	byte[] IORegisters;
+	byte[] extIORegisters;
+	byte[] internalSram;
 	
-	int[] dataBus;
+	byte dataBus;
 	int pc;
 	
 	
@@ -28,82 +32,92 @@ public class Uno {
 		digital = new Pin[14];
 		analog = new Pin[6];
 		power = new Pin[5];
-		flashMemory = new int[32768][8];
+		//32768 flash memory : 27648 for program, 5120 for boatloader
+		flashMemory_program = new byte[27648];
+		flashMemory_bootloader = new byte[5120];
 		
-		registers = new int[32][8];
-		IORegisters = new int[64][8];
-		extIORegisters = new int[160][8];
-		internalSram = new int[1792][8];
+		registers = new byte[32];
+		IORegisters = new byte[64];
+		extIORegisters = new byte[160];
+		internalSram = new byte[1792];
 		
-		eeprom = new int[1024][8];
+		eeprom = new byte[1024];
 		
-		dataBus = new int[8];
+		dataBus = 0;
 		pc = 0;
 
 	}
-	// X-Register getter
-	private int[] getXReg() {
-		int[] x = new int[16];
-		for (int i=0; i<8; i++) {
-			x[i] = registers[26][i];
-		}
-		for (int i=0; i<8; i++) {
-			x[8+i] = registers[27][i];
-		}
-		return x;
-	}
-	// X-Register setter
-	private void setXReg(int[] x) {
-		for (int i=0; i<8; i++) {
-			registers[26][i] = x[i];
-		}
-		for (int i=0; i<8; i++) {
-			registers[27][i] = x[8+i];
-		}
-	}
 	
-	// Y-Register getter
-	private int[] getYReg() {
-		int[] y = new int[16];
-		for (int i=0; i<8; i++) {
-			y[i] = registers[28][i];
-		}
-		for (int i=0; i<8; i++) {
-			y[8+i] = registers[29][i];
-		}
-		return y;
-	}
-	// Y-Register setter
-	private void setYReg(int[] x) {
-		for (int i=0; i<8; i++) {
-			registers[28][i] = x[i];
-		}
-		for (int i=0; i<8; i++) {
-			registers[29][i] = x[8+i];
-		}
-	}
 	
-	// Z-Register getter
-	private int[] getZReg() {
-		int[] z = new int[16];
-		for (int i=0; i<8; i++) {
-			z[i] = registers[30][i];
-		}
-		for (int i=0; i<8; i++) {
-			z[8+i] = registers[31][i];
-		}
-		return z;
+	public void LoadProgram() throws FileNotFoundException, IOException {
+		File program = new File("program.hex");
+		HexFileParser hfp = new HexFileParser(program);
+		flashMemory_program = hfp.parseFile();
 	}
-	// Z-Register setter
-	private void setZReg(int[] x) {
-		for (int i=0; i<8; i++) {
-			registers[30][i] = x[i];
-		}
-		for (int i=0; i<8; i++) {
-			registers[31][i] = x[8+i];
-		}
-	}
-	
+
+//	// X-Register getter
+//	private int[] getXReg() {
+//		int[] x = new int[16];
+//		for (int i=0; i<8; i++) {
+//			x[i] = registers[26][i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			x[8+i] = registers[27][i];
+//		}
+//		return x;
+//	}
+//	// X-Register setter
+//	private void setXReg(int[] x) {
+//		for (int i=0; i<8; i++) {
+//			registers[26][i] = x[i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			registers[27][i] = x[8+i];
+//		}
+//	}
+//	
+//	// Y-Register getter
+//	private int[] getYReg() {
+//		int[] y = new int[16];
+//		for (int i=0; i<8; i++) {
+//			y[i] = registers[28][i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			y[8+i] = registers[29][i];
+//		}
+//		return y;
+//	}
+//	// Y-Register setter
+//	private void setYReg(int[] x) {
+//		for (int i=0; i<8; i++) {
+//			registers[28][i] = x[i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			registers[29][i] = x[8+i];
+//		}
+//	}
+//	
+//	// Z-Register getter
+//	private int[] getZReg() {
+//		int[] z = new int[16];
+//		for (int i=0; i<8; i++) {
+//			z[i] = registers[30][i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			z[8+i] = registers[31][i];
+//		}
+//		return z;
+//	}
+//	// Z-Register setter
+//	private void setZReg(int[] x) {
+//		for (int i=0; i<8; i++) {
+//			registers[30][i] = x[i];
+//		}
+//		for (int i=0; i<8; i++) {
+//			registers[31][i] = x[8+i];
+//		}
+//	}
+//	
 	
 	// 16-bit Instructions
 	// each lines contains the following:-
@@ -113,7 +127,7 @@ public class Uno {
 		FileReader instructions = new FileReader(filename);
 		BufferedReader reader = new BufferedReader(instructions);
 //		:10 0000 00 0C946100 0C947E00 0C947E00 0C947E00 95
-		
+
 		
 		while(true) {
 			
@@ -143,5 +157,7 @@ public class Uno {
 		
 	}
 	
-	
+	public static void main(String[] args) throws IOException {
+
+	}
 }
